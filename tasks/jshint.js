b94a50a7b9a5ec2c6ba8bf02e4f6b084f1c32f4d
tillarnold <tillarnold@users.noreply.github.com>
2014-06-19 15:02:57 +0200

    Added tasks/jshint.js

---
+++ tasks/jshint.js
@@ -0,0 +1,22 @@
+var rewire = require('rewire');
+var proxyquire = require('proxyquire');
+var react = require('react-tools')
+ 
+var jshintcli = rewire('jshint/src/cli');
+ 
+var origLint = jshintcli.__get__("lint");
+ 
+jshintcli.__set__("lint", function myLint(code, results, config, data, file) {
+console.log("Linting file:",file);
+origLint(react.transform(code);, results, config, data, file);
+});
+ 
+var libJsHint = proxyquire('grunt-contrib-jshint/lib/jshint',{
+'jshint/src/cli': jshintcli
+});
+ 
+var gruntContribJshint = proxyquire('grunt-contrib-jshint',{
+'./lib/jshint': libJsHint
+});
+ 
+module.exports = gruntContribJshint;
