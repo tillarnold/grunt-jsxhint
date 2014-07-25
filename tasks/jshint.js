0114a19ec86a90874ff7b06a647f7755b1d578e4
Alexandra Morozova <astepanchuk@mirantis.com>
2014-07-25 13:29:30 +0300

    Added check for /** @jsx React.DOM */ comment in .jsx file  - added jstransform node module for checking  - added check in task file

--- tasks/jshint.js
+++ tasks/jshint.js
@@ -1,8 +1,9 @@
 var rewire = require('rewire');
 var proxyquire = require('proxyquire');
-var react = require('react-tools')
+var react = require('react-tools');
 
 var jshintcli = rewire('jshint/src/cli');
+var docblock = require('jstransform/src/docblock');
 
 //Get the original lint function 
 var origLint = jshintcli.__get__("lint");
@@ -11,10 +12,15 @@ var jsxSuffix = ".jsx";
 
 //override the lint function to also transform the jsx code
 jshintcli.__set__("lint", function myLint(code, results, config, data, file) {
-  var isJsxFile = file.indexOf(jsxSuffix, file.length - jsxSuffix.length) !== -1
-  if (isJsxFile) {
-    origLint(react.transform(code), results, config, data, file);
-  }
+    var isJsxFile = file.indexOf(jsxSuffix, file.length - jsxSuffix.length) !== -1;
+//added check for having /** @jsx React.DOM */ comment
+    var hasDocblock = docblock.parseAsObject(docblock.extract(code)).jsx;
+    if (isJsxFile && !hasDocblock) {
+        code = '/** @jsx React.DOM */' + code;
+    }
+    if (isJsxFile || hasDocblock) {
+        origLint(react.transform(code), results, config, data, file);
+    }
   else {
     origLint(code, results, config, data, file);
   }
