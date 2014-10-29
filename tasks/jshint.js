440cf04631effc0912a5db52c5331790500df575
tillarnold <throwable42@gmail.com>
2014-10-29 23:05:23 +0100

    Use single quotes

--- tasks/jshint.js
+++ tasks/jshint.js
@@ -12,13 +12,13 @@ var jshintcli = rewire('jshint/src/cli');
 var docblock = require('jstransform/src/docblock');
 
 //Get the original lint function 
-var origLint = jshintcli.__get__("lint");
+var origLint = jshintcli.__get__('lint');
 
-var jsxSuffix = ".jsx";
+var jsxSuffix = '.jsx';
 var reactJsSuffix = '.react.js';
 
 //override the lint function to also transform the jsx code
-jshintcli.__set__("lint", function myLint(code, results, config, data, file) {
+jshintcli.__set__('lint', function myLint(code, results, config, data, file) {
   var isJsxFile = file.indexOf(jsxSuffix, file.length - jsxSuffix.length) !== -1;
   var isReactJsFile = file.indexOf(reactJsSuffix, file.length - reactJsSuffix.length) !== -1;
   var hasSuffix = isJsxFile || isReactJsFile;
